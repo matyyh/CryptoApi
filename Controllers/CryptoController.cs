92c35c81b6be0ac1f4b9546259ea839a10e8f8c2
matyyh <matyxx@protonmail.com>
2026-03-03 22:29:03 +0000

    Add files via upload

---
+++ Controllers/CryptoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoController : ControllerBase
+    {
+        private readonly int _shift = 3;
+
+        [HttpGet("encrypt/{text}")]
+        public IActionResult Encrypt(string text)
+        {
+            var result = new string(text.Select(c => (char)(c + _shift)).ToArray());
+            return Ok(new { original = text, result = result });
+        }
+
+        [HttpGet("decrypt/{text}")]
+        public IActionResult Decrypt(string text)
+        {
+            var result = new string(text.Select(c => (char)(c - _shift)).ToArray());
+            return Ok(new { original = text, result = result });
+        }
+    }
+}
+
